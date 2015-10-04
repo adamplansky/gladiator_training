9fc3fa29c66ff4358b88a618cf0161f1c2601422
Adam Plansky <adamplansky@gmail.com>
2015-10-04 20:56:47 +0200

    order_by model reser

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,7 +4,7 @@ class Reservation < ActiveRecord::Base
   has_many :reservation_users
   belongs_to :place
   belongs_to :user
-  
-
+    
+  default_scope {order('time_from')}
   validates :capacity, numericality: { only_integer: true }
 end
