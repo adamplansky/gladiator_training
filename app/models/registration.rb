0a5874701573cc2d6c2251662fb6428a15a499f4
Adam Plansky <adamplansky@gmail.com>
2016-05-17 09:58:17 +0200

    registrations order by asc + _show add created_at

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,7 +1,8 @@
 class Registration < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
-
+  default_scope -> { order('created_at asc') }
+  
   before_create :set_registration_time_to_now
   def set_registration_time_to_now
     self.registration_time = Time.now
