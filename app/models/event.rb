b7d58dd449a91e4ef11ba917e1e2279a792eadb4
Adam Plansky <adamplansky@gmail.com>
2016-05-17 09:46:49 +0200

    event validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,9 @@ class Event < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   has_many :users, through: :registration
   has_many :registration
-
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :place, presence: true
+  validates :place_url, presence: true
+  validates :image, presence: true
 end
