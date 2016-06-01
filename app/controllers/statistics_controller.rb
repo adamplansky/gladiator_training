79f456955175b11890305b4c6a04c750cb3fe65f
Adam Plansky <adamplansky@gmail.com>
2016-06-01 08:32:25 +0200

    statistiss -> period_id add

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,13 +3,14 @@ class StatisticsController < ApplicationController
   before_action :logged_in_user
   def index
     @categories = TrainingCategory.all
+    @period = Period.where("time_from < ? AND time_to > ?", Time.now, Time.now).first
     @users = User.all
     @results = {}
     @categories.each do |category|
-      tsum = Training.unscope(:order).joins(:user).where(training_category: category.id).group("user_id").order("sum_points DESC").sum(:points).take(5)
-      @results[category] = Training.unscope(:order).joins(:user).where(training_category: category.id).group("user_id").order("sum_points DESC").sum(:points).take(5) unless tsum.empty?
+      tsum = Training.unscope(:order).joins(:user).where(training_category: category.id, period_id: @period).group("user_id").order("sum_points DESC").sum(:points).take(5)
+      @results[category] = Training.unscope(:order).joins(:user).where(training_category: category.id, period_id: @period).group("user_id").order("sum_points DESC").sum(:points).take(5) unless tsum.empty?
     end
-    @sum_all_points =  Training.unscope(:order).joins(:user).group("user_id").order("sum_points DESC").sum(:points).take(10)
+    @sum_all_points =  Training.unscope(:order).joins(:user).where(period_id: @period).group("user_id").order("sum_points DESC").sum(:points).take(10)
 
   end
 
