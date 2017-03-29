99b80c6cf20db82be92372496b2007fa028a100f
Adam Plansky <adamplansky@gmail.com>
2017-03-29 10:05:15 +0200

    nav-tabs challenge-leaderboards

--- app/controllers/challenge_leaderboards_controller.rb
+++ app/controllers/challenge_leaderboards_controller.rb
@@ -1,8 +1,8 @@
 class ChallengeLeaderboardsController < ApplicationController
   layout "gymwars"
-  before_action :set_challenge, only: [:men, :women, :gyms, :mixes]
+  before_action :set_challenge, only: [:men, :women, :gyms, :mixes, :index]
   def index
-    render plain: "ChallengeLeaderboardsController"
+    @data_set = Pgdb.get_gym_points_gyms_wars(@challenge.id)
   end
 
   def men
@@ -18,24 +18,13 @@ class ChallengeLeaderboardsController < ApplicationController
   end
 
   def mixes
-
   end
 
   private
+
   def set_challenge
     @challenge = Challenge.find(params[:challenge_id])
     @users = User.all
     @gyms = Gym.all
   end
-  # @challenges = Challenge.all
-  # @users = User.all
-  # @challenge_ary = []
-  # @challenges.each do |challenge|
-  #   @challenge_ary << {
-  #     challenge: challenge,
-  #     male: Pgdb.get_points_gyms_wars(Gender::Male, challenge.id),
-  #     female: Pgdb.get_points_gyms_wars(Gender::Female, challenge.id),
-  #   }
-  #
-  # end
 end
