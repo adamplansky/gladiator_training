e0e012a14926c86636e4acff1bbc245160630cac
Adam Plansky <adamplansky@gmail.com>
2017-04-03 16:43:52 +0200

    user change!!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   validates :surname, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },   uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :gender, presence: true
   attr_accessor :remember_token, :activation_token, :reset_token
   has_secure_password
