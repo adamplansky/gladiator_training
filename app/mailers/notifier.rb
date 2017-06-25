5461d42d70512985a59202b3fa3b5e4e2a722f28
Adam Plansky <adamplansky@gmail.com>
2017-06-25 20:12:07 +0200

    multiple email registration

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -9,11 +9,7 @@ class Notifier < ApplicationMailer
    def race_registration(user)
      puts user.inspect
      @u = user
-     mail(to: user.email, subject: "Potvrzení registrace Gladiator Challenge")
-     mail(to: "adamplansky@gmail.com", subject: "registrace Gladiator Challenge")
-     mail(to: "hege8400@seznam.cz", subject: "registrace Gladiator Challenge")
-     mail(to: "jiricimler@centrum.cz", subject: "registrace Gladiator Challenge")
-
+     mail(to: "#{user.email},adamplansky@seznam.cz,hege8400@seznam.cz,jiricimler@centrum.cz", subject: "registrace Gladiator Challenge")
    end
 
 end
