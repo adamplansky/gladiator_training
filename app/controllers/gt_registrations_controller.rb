3f030764e66a538adc009026b4b5f0edd995fde9
Adam Plansky <adamplansky@gmail.com>
2017-07-24 13:12:10 +0200

    GtRegistration.order(:id) payed_with_index

--- app/controllers/gt_registrations_controller.rb
+++ app/controllers/gt_registrations_controller.rb
@@ -10,7 +10,7 @@ class GtRegistrationsController < ApplicationController
   end
 
   def index_with_payed
-    @gt_registrations = GtRegistration.all
+    @gt_registrations = GtRegistration.order(:id)
   end
 
   # GET /gt_registrations/1
